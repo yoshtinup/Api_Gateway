16867640e25eb8cdeda72c4bdf2e6a353d7702e3
yoshtinup <yoshtingutierrez@gmail.com>
2025-07-14 03:06:03 -0600

    cambio9

--- src/infrastructure/services/productoService.js
+++ src/infrastructure/services/productoService.js
@@ -1,21 +1,21 @@
 const axios = require('axios');
 
 exports.crearProducto = async (producto) => {
-  const response = await axios.post('http://localhost:3002/api/v1/producto', producto);
+  const response = await axios.post('http://3.230.107.32:3002/api/v1/producto', producto);
   return response.data;
 };
 exports.obtenerProductoPorId = async (id) => {
-  const response = await axios.get(`http://localhost:3002/api/v1/producto/${id}`);
+  const response = await axios.get(`http://3.230.107.32:3002/api/v1/producto/${id}`);
   return response.data;
 };
 
 
 exports.actualizarProducto = async (id, producto) => {
-  const response = await axios.put(`http://localhost:3002/api/v1/producto/${id}`, producto);
+  const response = await axios.put(`http://3.230.107.32:3002/api/v1/producto/${id}`, producto);
   return response.data;
 }
 
 exports.eliminarProducto = async (id) => {
-  const response = await axios.delete(`http://localhost:3002/api/v1/producto/${id}`);
+  const response = await axios.delete(`http://3.230.107.32:3002/api/v1/producto/${id}`);
   return response.data;
 };
